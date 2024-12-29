588c6e62739dd6fe6cde8e2791d01f21488b22fb
Rob J Goedman <goedman@icloud.com>
2024-12-29 07:37:15 -0700

    Initial Commit

---
+++ MyRecallAppTests/MyRecallAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecallAppTests.swift
+//  MyRecallAppTests
+//
+//  Created by Robert Goedman on 12/29/24.
+//
+
+import Testing
+@testable import MyRecallApp
+
+struct MyRecallAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
